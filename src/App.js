00beee290df5666df0a6b64ee4515f6c68018b94
Bkaes <bkaes8@gmail.com>
2020-03-07 19:47:41 -0500

    Moved to components and pages

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@ import React, {Component} from "react";
 import "./App.css";
 import {Navbar, Nav, NavItem} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import Routes from "./Routes";
+import Routes from "./Components/Routes/Routes";
+import Navigation from './Components/Navigation/Navigation';
 
 export default class App extends Component {
   render() {
     return (
-    <div className="App container">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand>
-          <Link to="/">Twin Aviation</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <NavItem>
-              <Nav.Link href="/MySchedule">MySchedule</Nav.Link>
-              <Nav.Link href="/Reserve">Reserve</Nav.Link>
-              <Nav.Link href="/Weather">Weather</Nav.Link>
-              <Nav.Link href="/LogBook">LogBook</Nav.Link>
-              <Nav.Link href="/Logout">Logout</Nav.Link>
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-        
-      </Navbar>
+    <div className="App-Container">
+      <Navigation/>
       <Routes />
     </div>
     );
